1cc30c21b31c3b916d57c66ba9dd7373cff5839f
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2024-01-24 16:23:13 +0100

    Built the pipeline in main()

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 '''This module contains main logic of the pipeline.'''
 
+from src.ETL import extract, transform, load
+
 def main():
-    pass
+    df1, df2, df3 = extract(['UCKq-lHnyradGRmFClX_ACMw'])
+    final_df = transform(df1, df2, df3)
+    load(final_df, 'monthly_metrics')
 
 if __name__ == "__main__":
     main()
